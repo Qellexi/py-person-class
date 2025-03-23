e1c7b6d9b52f559d1d7181dddeee6f85e3bcc88b
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-03-23 20:19:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people_dict = {}
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        Person.people_dict[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_list = []
+
+    for person in people:
+        person_in_list = Person(person["name"], person["age"])
+        people_list.append(person_in_list)
+    for person in people:
+        wife_name = person.get("wife")
+        husband_name = person.get("husband")
+        if wife_name:
+            Person.people_dict[person["name"]].wife = Person.people_dict[person["wife"]]
+        elif husband_name:
+            Person.people_dict[person["name"]].husband = Person.people_dict[person["husband"]]
+
+    return people_list
